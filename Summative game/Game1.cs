32e2084175b05b4597e10ddb44a0f100a7cea7b1
Nathan Barnes <339723215@GoTVDSB.ca>
2024-12-13 09:34:33 -0500

    Add project files.

---
+++ Summative game/Game1.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Summative_game
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D target;
+        Texture2D crosshair;
+        Rectangle targetRect;
+        Rectangle crosshairRect;
+        Rectangle window;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+            crosshairRect = new Rectangle(200, 200, 150, 150);
+            targetRect = new Rectangle(350, 350, 150, 150);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            target = Content.Load<Texture2D>("target");
+            crosshair = Content.Load<Texture2D>("crosshair");
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(target, targetRect, Color.White);
+            _spriteBatch.Draw(crosshair, crosshairRect, Color.White);
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Summative game/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Summative_game.Game1();
+game.Run();
