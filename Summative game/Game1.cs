48a8f2f7deeb5081b9685b52143c5a824a868d9e
Nathan Barnes <339723215@GoTVDSB.ca>
2024-12-16 09:40:58 -0500

    d

--- Summative game/Game1.cs
+++ Summative game/Game1.cs
@@ -5,17 +5,30 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Summative_game
 {
+    enum Screen
+    {
+        Intro,
+        Controls,
+        Play,
+        Lose,
+        Win
+    }
+
+
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        Texture2D Intro;
         Texture2D target;
         Texture2D crosshair;
         Rectangle targetRect;
         Rectangle crosshairRect;
         Rectangle window;
         MouseState mouseState;
+        Screen screen;
+        SpriteFont font;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -30,9 +43,10 @@ namespace Summative_game
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
-            crosshairRect = new Rectangle(310, 238, 150, 150);
+            crosshairRect = new Rectangle(330, 210, 150, 150);
             targetRect = new Rectangle(350, 350, 150, 150);
             base.Initialize();
+            screen = Screen.Intro;
         }
 
         protected override void LoadContent()
@@ -40,6 +54,8 @@ namespace Summative_game
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             target = Content.Load<Texture2D>("target");
             crosshair = Content.Load<Texture2D>("crosshair");
+            Intro = Content.Load<Texture2D>("Intro");
+            font = Content.Load<SpriteFont>("font");
             // TODO: use this.Content to load your game content here
         }
 
@@ -59,8 +75,17 @@ namespace Summative_game
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
-            _spriteBatch.Draw(target, targetRect, Color.White);
-            _spriteBatch.Draw(crosshair, crosshairRect, Color.White);
+            if (screen == Screen.Play)
+            {
+                _spriteBatch.Draw(target, targetRect, Color.White);
+                _spriteBatch.Draw(crosshair, crosshairRect, Color.White);
+            }
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(Intro, window, Color.White);
+                _spriteBatch.DrawString(font, "Left click to play", new Vector2(550, 300), Color.White);
+            }
+            
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
